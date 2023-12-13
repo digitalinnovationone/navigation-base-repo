44dfc0bf76f8a417f0e395b12f113b29b60c3f47
FelipeAguiarCode <felipe.silva.aguiar047@gmail.com>
2023-12-13 11:34:04 -0300

    projeto criado

---
+++ src/screens/GalleryScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function GalleryScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "red" }}>
+      <Text>Home works!!</Text>
+    </View>
+  );
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "red" }}>
+      <Text>Home works!!</Text>
+    </View>
+  );
+}
